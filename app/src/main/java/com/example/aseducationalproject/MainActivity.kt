edb1017853ef9c06820ce157132c79e95a9e5eac
fhntva <fhntv.apt@yandex.ru>
2025-03-26 22:13:03 +0300

    RA07 базовая верстка экрана категории (item Category)

--- app/src/main/java/com/example/aseducationalproject/MainActivity.kt
+++ app/src/main/java/com/example/aseducationalproject/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.aseducationalproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.aseducationalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,16 +28,37 @@ class MainActivity : AppCompatActivity() {
                 add<CategoriesListFragment>(R.id.mainContainer)
             }
 
-            enableEdgeToEdge()
-            setContentView(R.layout.activity_main)
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-
+        binding.btnCategoryMain.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<CategoriesListFragment>(R.id.mainContainer)
+                setReorderingAllowed(true)
+                addToBackStack("name")
             }
+        }
+            binding.btnFavoritesMain.setOnClickListener {
+                supportFragmentManager.commit {
+                    replace<FragmentFavorites>(R.id.mainContainer)
+                    setReorderingAllowed(true)
+
+                }
+            }
+
 
 
         }
     }
+
 }
+    //            enableEdgeToEdge()
+//            setContentView(R.layout.activity_main)
+//            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//                insets
+
+
+
+
+
+
+
