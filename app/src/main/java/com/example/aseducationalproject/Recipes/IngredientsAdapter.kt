d1b96d18a88bfb10e758825077c0591a959a728c
VladWhite22 <vladislav22971@mail.ru>
2025-06-04 22:04:30 +0300

    RA-14 Добавить SeekBar для расчета ингредиентов

--- app/src/main/java/com/example/aseducationalproject/Recipes/IngredientsAdapter.kt
+++ app/src/main/java/com/example/aseducationalproject/Recipes/IngredientsAdapter.kt
@@ -7,30 +7,41 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.aseducationalproject.Domain.Ingredient
 import com.example.aseducationalproject.databinding.ItemIngredientBinding
 
-class IngredientsAdapter(private val dataSet: List<Ingredient>):
+
+class IngredientsAdapter(private val dataSet: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+    private var quantity: Double = 1.0
 
     class ViewHolder(binding: ItemIngredientBinding) : RecyclerView.ViewHolder(binding.root) {
         val textView: TextView = binding.tvIngredient
         val quantity: TextView = binding.tvQuantity
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val binding =
             ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val ingredient: Ingredient = dataSet[position]
-
         holder.textView.text = ingredient.description
-        holder.quantity.text = "${ingredient.quantity} ${ingredient.unitOfMeasure}"
-
+        var updateQuant = ingredient.quantity.toBigDecimal().multiply(quantity.toBigDecimal())
+            .stripTrailingZeros().toPlainString()
+        holder.quantity.text = "${updateQuant} ${ingredient.unitOfMeasure}"
     }
 
     override fun getItemCount(): Int = dataSet.size
+
+    fun updateIngredients(progress: Double) {
+        quantity = progress
+        notifyDataSetChanged()
+    }
+
+    companion object
 
 
 }
